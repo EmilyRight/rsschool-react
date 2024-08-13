cadf6ffe6ca9171e05d0f494bd16a17b73d84c36
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2024-08-14 01:18:59 +0300

    fix: delete eslintrc

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { ESLint } from 'eslint';
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+export default new ESLint({
+  baseConfig: {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ],
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error'
+    }
+  },
+});
